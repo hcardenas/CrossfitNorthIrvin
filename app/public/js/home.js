2cad20cc3357b9dcdb00cd8de48dd3f46b891541
Helmut Cardenas <aleksey89col@gmail.com>
2018-01-18 22:48:04 -0600

    added a settimeout to tap

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -2,6 +2,12 @@ $( document ).ready(function() {
     $('.tap-target').tapTarget('open');
     $(".dropdown-button").dropdown();
     $('.slider').slider();
+
+    setTimeout( function() { 
+    	$('.tap-target').tapTarget('close'); 
+
+    }, 3000);
+
 });
 
 
